1b599c116349554ae8f4d0ab9a50f5b165722510
Duong Vu <vu.t.duong01@gmail.com>
2024-12-26 11:51:33 -0500

    Update level 51

--- Assets/Scripts/Floor/FloorLava.cs
+++ Assets/Scripts/Floor/FloorLava.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class FloorLava : Floor
 {
     [Header("Effects References")]
     [SerializeField] private AudioSource explosionAudio;
+    [SerializeField] private new Light2D light;
 
     #region Unity Events
+
+    protected override void Start()
+    {
+        base.Start();
+        var size = mainSprite.size;
+        light.SetShapePath(new Vector3[] {
+            new(-size.x / 2f, size.y / 2f),
+            new(size.x / 2f, size.y / 2f),
+            new(size.x / 2f, -size.y / 2f),
+            new(-size.x / 2f, -size.y / 2f),
+         });
+    }
 
     private void Update()
     {
