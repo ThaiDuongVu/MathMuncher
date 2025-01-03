992dcb3aeafd61736360c4d209ec713afab7c3f6
Duong Vu <vu.t.duong01@gmail.com>
2025-01-03 09:10:01 -0500

    Add level 55

--- Assets/Scripts/Actor/Interactable/LightBulb.cs
+++ Assets/Scripts/Actor/Interactable/LightBulb.cs
@@ -1,7 +1,26 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class LightBulb : Interactable
 {
+    [Header("Light Bulb References")]
+    [SerializeField] private float revealRadius = 1.5f;
+    [SerializeField] private Light2D revealLight;
+    [SerializeField] private SpriteMask revealCircle;
+
+    #region Unity Events
+
+    protected override void Start()
+    {
+        base.Start();
+
+        // Update reveal radius
+        revealLight.pointLightInnerRadius = revealLight.pointLightOuterRadius = revealRadius;
+        revealCircle.transform.localScale = Vector2.one * revealRadius * 2f;
+    }
+
+    #endregion
+
     public override bool OnInteracted(Actor actor)
     {
         return false;
